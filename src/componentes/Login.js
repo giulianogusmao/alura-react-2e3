e82910b1c48d8cae09d41c6de2d13deb002c263f
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-07 15:22:22 -0300

    Criando logout

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -4,11 +4,11 @@ import Helper from '../_helper/helper';
 
 export default class Login extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            msg: '',
+            msg: (this.props.location.query['msg'] || ''),
         };
     }
 
@@ -52,7 +52,7 @@ export default class Login extends Component {
             <div className="login-box">
                 <h1 className="header-logo">Instalura</h1>
                 <form onSubmit={this.enviaForm.bind(this)}>
-                    <span></span>
+                    <span className="text-msg">{this.state.msg}</span>
                     <input type="text" ref={input => this.login = input} />
                     <input type="password" ref={input => this.senha = input} />
                     <input type="submit" value="login" />
--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ import './css/login.css';
 import './css/timeline.css';
 import App from './App';
 import Login from './componentes/Login';
+import Logout from './componentes/Logout';
 import { Router, Route, browserHistory } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
+
+function verificaAutenticacao(nextStep, replace) {
+    if (localStorage.getItem('auth-token') === null) {
+        replace('/?msg=Você precisa estar logado para acessar o endereço');
+    }
+}
 
 ReactDOM.render(
     (
         <Router history={browserHistory}>
             <Route path="/" component={Login} />
-            <Route path="/timeline" component={App} />
+            <Route path="/timeline" component={App} onEnter={verificaAutenticacao} />
+            <Route path="/logout" component={Logout} />
         </Router>
     ),
     document.getElementById('root')
